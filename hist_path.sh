5c1131d7e25543b661fc29de05f606f3ee21be1c
Dave Larson <dlarson@genome.wustl.edu>
2016-08-04 09:53:35 -0500

    remove shell scripts that are no longer needed. scripts referenced in wdl are at https://github.com/ernfrid/svtools_bash_wrappers.git

--- hist_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -uex
-
-SAMPLE=$1
-LUMPY_DIR=$2
-BAM=$3
-
-HIST=$LUMPY_DIR/$SAMPLE/temp/cnvnator-temp/${BAM##*/}.hist.root
-echo $HIST
--- splitter_bam_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -uex
-
-SAMPLE=$1
-BAM=$2
-
-#echo -e "Sample\tBAM\tSPLITTER"
-SPL=${BAM%.*}.splitters.bam
-echo $SPL
